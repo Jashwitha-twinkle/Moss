047edd866cd4f1a706e1abe8e584097bce8a894b
Rimuru <jashwithabhandary@gmail.com>
2025-08-19 14:52:22 +0530

    Made changes for deployment on render instead of vercel

--- moss/build_files.sh
+++
@@ -1,5 +0,0 @@
-# build_files.sh - Put this in your project root
-echo "BUILD START"
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
-echo "BUILD END"
